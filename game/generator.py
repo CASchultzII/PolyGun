6552878540352513ea8d55670214b89805994786
CASchultzII <c.a.schultz.ii@gmail.com>
2016-08-30 16:31:45 -0600

    Shapes fall at a reasonable pace now. :P

--- game/generator.py
+++ game/generator.py
@@ -2,6 +2,7 @@
 
 import random
 from game.projectile import ShapeEnum, TypeEnum
+from game.tools import Constants
 
 """
 Contains the generation logic for creating new shapes.
@@ -11,16 +12,22 @@ class TargetGenerator():
     def __init__(self, pool):
         self.pool = pool
         
-        self.shapeChance = .2
+        self.shapeChance = .01
+        self.shapeCooldown = 500 # ms
 
     """ Generator may create new shapes on a call to update. """
     def update(self):
-        if (random.random() >= self.shapeChance): # spawn new shape!
-            shape = random.choice((ShapeEnum.CIRCLE, ShapeEnum.SQUARE, ShapeEnum.TRIANGLE))
+        self.shapeCooldown -= Constants.clock.get_time()
+        if self.shapeCooldown < 0: self.shapeCooldown = 0
+        
+        if (self.shapeCooldown <= 0):
+            if (random.random() <= self.shapeChance): # spawn new shape!
+                shape = random.choice((ShapeEnum.CIRCLE, ShapeEnum.SQUARE, ShapeEnum.TRIANGLE))
             
-            xpos = random.randrange(16, 600-112)
-            ypos = -70
+                xpos = random.randrange(16, 600-112)
+                ypos = -70
             
-            velocity = 250
+                velocity = 250
             
-            self.pool.generate(shape, TypeEnum.TARGET, [xpos, ypos], velocity, 0)
+                self.pool.generate(shape, TypeEnum.TARGET, [xpos, ypos], velocity, 0)
+                self.shapeCooldown = 500
