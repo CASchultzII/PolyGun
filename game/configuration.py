280cf88e1fd897362bcae8f8803c21d5d973cfbe
joycechendev <superbrainchen@gmail.com>
2016-08-30 10:34:10 -0600

    Updated Configuration

--- game/configuration.py
+++ game/configuration.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
 
-import configparser
+from configparser import ConfigParser
 
 """
 Holds the configuration options for the game.
 """
-class Configuration(configparser):
+class Configuration(ConfigParser):
     
     """Gets the Targets per Second"""
-    def getTPS():
-        pass
-
+    def getTPS(self):
+        self.read('settings.ini')
+        return self['DEFAULT']['TargetsPerSecond']
+        
     """Gets the Targets per Second Squared (Acceleration)"""
-    def getTPS2():
-        pass
+    def getTPS2(self):        
+        self.read('settings.ini')
+        print (self['DEFAULT']['TargetsPerSecondSqr'])
 
     """Gets the Bullets per Second (cooldown for firing rate)"""
-    def getBPS():
-        pass
+    def getBPS(self):
+        self.read('settings.ini')
+        return self['DEFAULT']['BulletsPerSecond']
+
+
+def main():
+    cfg = Configuration()
+    cfg.getTPS()
+
+main()
