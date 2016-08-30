9ac835f271e1ae7e41698bc46377eabc90af158d
CASchultzII <c.a.schultz.ii@gmail.com>
2016-08-30 09:40:17 -0600

    Made some initial progress on code base.

--- game/player.py
+++ game/player.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python3
 
+import game.projectile.ShapeEnum
+
 """
 Contains information on the current player instance.
 """
 class PlayerInfo:
 
-    """ Asks the PlayerInfo to try to fire. """
-    def fire():
-        pass
+    """ Init this Player """
+    def __init__(self, sprite, pool): #BAD COUPLING
+        self.timeScore = 0L
+        self.timeCooldown = 0L
+        self.resources = {ShapeEnum.CIRCLE: 10, ShapeEnum.SQUARE: 10, ShapeEnum.TRIANGLE: 10}
+        self.position = 0 # TODO create a world coordinate transform
+        self.sprite = sprite
+        self.pool = pool # BAD COUPLING
 
+    """ Asks the PlayerInfo to try to fire. """
+    def fire(self):
+        pass
+        
     """ Checks if the player collides with the provided rect. """
-    def collides(rect):
+    def collides(self, rect):
         pass
 
     """ Updates the player. """
-    def update():
+    def update(self):
         pass
 
     """ Draws the player (including resource HUD and time) """
-    def draw():
+    def draw(self):
         pass
--- main.py
+++ main.py
@@ -1,3 +1,40 @@
 #!/usr/bin/env python3
 
-""" PolyGun setup and main loop. """
+import sys
+import pygame
+pygame.init()
+
+from game import configuration, generator, player, projectile
+
+""" Contains game sensitive information. """
+class Game:
+
+    def __init__(self):
+        #self.config = new Configuration("path/to/config.cfg")
+        size = 1600, 900 # This should be relocated to configuration.py
+        self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN|pygame.HWSURFACE|pygame.DOUBLEFUB)
+
+    """ PolyGun setup. """
+    def init(self):
+        self.player = new player.PlayerInfo()
+        self.pool = new projectile.ProjectilePool()
+        self.tgen = new generator.TargetGenerator()
+
+    """ Update game. """
+    def update(self):
+        self.player.update()
+        self.pool.update()
+        self.tgen.update()
+
+    """ Draw game. """
+    def draw(self):
+        # draw background first
+        self.player.draw()
+        self.pool.draw()
+
+Game game = new Game()
+game.init()
+
+while (True): # need to add timing controls here using pygame.time.clock
+    game.update()
+    game.draw()
