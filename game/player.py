6eb47c20bd80bd2aaccb2e2d764f527d3ba433c3
CASchultzII <c.a.schultz.ii@gmail.com>
2016-08-30 12:13:31 -0600

    MOVABLE PLAYER!!! :D

--- game/player.py
+++ game/player.py
@@ -6,11 +6,6 @@ import pygame
 from game.projectile import ShapeEnum, TypeEnum
 from game.tools import Constants
 
-class Direction(Enum):
-    NONE = 0,
-    LEFT = 1,
-    RIGHT = 2
-
 """
 Contains information on the current player instance.
 """
@@ -23,8 +18,10 @@ class PlayerInfo:
         self.resources = {ShapeEnum.CIRCLE: 10, ShapeEnum.SQUARE: 10, ShapeEnum.TRIANGLE: 10}
         self.position = [236, 820] # TODO create a world coordinate transform
         self.sprite = pygame.image.load(os.path.join("assets", "cannon.png")).convert_alpha()
-        self.dir = Direction.NONE
         self.pool = pool # BAD COUPLING
+        
+        self.moveLeft = False
+        self.moveRight = False
 
     """ Asks the PlayerInfo to try to fire. """
     def fire(self, shapeEnum):
@@ -44,13 +41,13 @@ class PlayerInfo:
             if (self.timeCooldown < 0):
                 self.timeCooldown = 0
                 
-        mod = 0
-        if (self.dir != Direction.NONE):
-            mod = -1 if self.dir == Direction.LEFT else 1
-            self.dir = Direction.NONE
+        if (not (self.moveLeft or self.moveRight)):
+            return
+            
+        mod = -1 if self.moveLeft else 1
         
-        velocity = 100 * mod # pixels per second
-        self.position[0] = velocity * (Constants.clock.get_time() / 1000.0)
+        velocity = 600 * mod # pixels per second
+        self.position[0] += velocity * (Constants.clock.get_time() / 1000.0)
         if self.position[0] < 0:
             self.position[0] = 0
         elif self.position[0] > 600 - 128:
@@ -58,6 +55,7 @@ class PlayerInfo:
 
         print("Player Velocity: " + str(velocity) + "\n")
         print("Clock Delta: " + str(Constants.clock.get_time()/1000.0) + "\n" )
+        print("Move Delta: " + str(velocity * Constants.clock.get_time() / 1000.0) + "\n")
     """ Draws the player (including resource HUD and time) """
     def draw(self):
         # TODO add drawing of resource HUD and time
